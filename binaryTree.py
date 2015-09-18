2b8138d1a6bd8379f5899f4e51037ed2ef84e113
Senjoey <tq14@software.nju.edu.cn>
2015-09-18 17:01:03 +0800

    哈哈～🍑桃子
    一起嗨～

---
+++ binaryTree.py
@@ -0,0 +1,23 @@
+class binaryTree(object):
+    def _init_(self, value):
+        self.value = value
+        self.leftBranch = None
+        self.rightBranch = None
+        self.parent = None
+    def setLeftBranch(self, node):
+        self.leftBranch = node
+    def serRightBranch(self, node):
+        self.rightBranch = node
+    def setParent(self, parent):
+        self.parent = parent
+    
+    def getValue(self):
+        return self.value
+    def getLeftBranch(self):
+        return self.leftBranch
+    def getRightBranch(self):
+        return self.rightBranch
+    def getParent(self):
+        return self.parent
+    def _str_(self):
+        return self.value
